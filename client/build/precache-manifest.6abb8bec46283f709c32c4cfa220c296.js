e0161dca08ff94e5a089484132edba1ca7153650
unknown <krishnaprasanna1995@gmail.com>
2020-04-08 21:29:24 +0530

    build created

---
+++ client/build/precache-manifest.6abb8bec46283f709c32c4cfa220c296.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a9244a72d17fbb6c3b3f622d547677",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4c0e43ad5442a8757e0",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ad082c1045e265167d1d",
+    "url": "/static/css/main.14f5e673.chunk.css"
+  },
+  {
+    "revision": "c4c0e43ad5442a8757e0",
+    "url": "/static/js/2.7aba5915.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.7aba5915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad082c1045e265167d1d",
+    "url": "/static/js/main.a3fd4f5d.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const router = require("./config/route");
 const setUpDb = require("./config/database");
 const cors = require("cors");
@@ -8,6 +8,14 @@ app.use(express.json());
 app.use(cors());
 app.use("/", router);
 setUpDb();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log("App Started ", PORT);
 });
